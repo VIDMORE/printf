b62c9c12532a592cf3da58bf31c8a9b18b2c25e0
kajoomba99 <steven.brand1999@gmail.com>
2020-03-17 11:13:19 +0000

    main para probar

--- main.c
+++ main.c
@@ -8,7 +8,25 @@
  * Return: Always 0
  */
 int main(void)
-{ 
-   _printf("hol%c %s %i\n",'a', "como estas?", 234);
-    return (0);
+{
+	int len;
+	int len2;
+	unsigned int ui;
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	ui = (unsigned int)INT_MAX + 1024;
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
+	_printf("Negative:[%d]\n", -762534);
+	printf("Negative:[%d]\n", -762534);
+	_printf("Unsigned hexadecimal:[%x, %x]\n", ui, ui);
+	printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
+	len = _printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%]\n");
+	_printf("Len:[%d]\n", len);
+	printf("Len:[%d]\n", len2);
 }
