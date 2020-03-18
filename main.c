6999f504b5fda5eedd10ebbfaaefca9bced65450
David Corredor <vidmore8@gmail.com>
2020-03-18 03:41:16 +0000

    adding the new issues

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <limits.h>
+
+int main()
+{
+	_printf("%i\n", INT_MAX);
+	_printf("%i\n", INT_MIN);
+	_printf("%d\n", INT_MAX);
+	_printf("%d\n", INT_MAX);
+
+	return (0);
+}
